c700dc227204a9c4d6a27d3bb647199fd8d77c85
robertbutacu <robertbutacu@gmail.com>
2019-12-24 09:26:09 +0000

    basket total price

--- scala/src/main/scala/checkout/v1/services/CheckoutService.scala
+++ scala/src/main/scala/checkout/v1/services/CheckoutService.scala
@@ -1,15 +1,27 @@
 package checkout.v1.services
 
-import checkout.v1.models.{Item, Price}
+import checkout.v1.models.{Discount, Item, Price}
 
 trait CheckoutServiceAlgebra {
-  def basketTotalPrice(items: List[Item]): Price
+  def basketTotalPrice(items: List[Item], availableDiscounts: Set[Discount]): Price
 }
 
 class CheckoutService extends CheckoutServiceAlgebra {
-  override def basketTotalPrice(item: List[Item]): Price = {
-    import eu.timepit.refined.auto._
+  override def basketTotalPrice(item: List[Item], availableDiscounts: Set[Discount]): Price = {
+    val groupedItems: Map[String, List[Item]] = item.groupBy(_.name)
 
-    item.foldLeft(Price(0, 0))((total, current) => Price.add(total, current.price))
+    val discountsForBasket = availableDiscounts.filter {
+      d =>
+        groupedItems.exists {
+          case (itemName, items) => itemName == d.item.name && items.size >= d.count.value
+        }
+    }
+
+    val discountAmount = discountsForBasket.foldLeft(Price())((total, current) => Price.add(total, current.discountAmount))
+
+    val basketTotal = item.foldLeft(Price())((total, current) => Price.add(total, current.price))
+
+    //Price.minus(basketTotal, discountAmount)
+    ???
   }
 }
--- scala/src/test/scala/checkout/v1/services/CheckoutServiceSpec.scala
+++ scala/src/test/scala/checkout/v1/services/CheckoutServiceSpec.scala
@@ -1,21 +1,25 @@
 package checkout.v1.services
 
-import checkout.v1.models.{Item, Price}
+import checkout.v1.models.{Discount, Item, Price}
 import eu.timepit.refined.auto._
 import org.scalatest.{Matchers, WordSpec}
 
 class CheckoutServiceSpec extends WordSpec with Matchers {
-  val checkoutService: CheckoutServiceAlgebra = new CheckoutService
+  val item = Item("some-random-name", Price(20))
+  val someRandomItemDiscount: Discount = Discount(item, 3, Price(5)).get
 
+  val checkoutService: CheckoutServiceAlgebra = new CheckoutService
   "basketTotalPrice" should {
     "correctly price a list of items with 1 element" in {
-      val item = Item("some-random-name", Price(20, 0))
-      checkoutService.basketTotalPrice(List(item)) shouldBe Price(20, 0)
+      checkoutService.basketTotalPrice(List(item), Set.empty) shouldBe Price(20)
     }
 
     "correctly sum a list of same items" in {
-      val item = Item("some-random-name", Price(20, 0))
-      checkoutService.basketTotalPrice(List(item, item)) shouldBe Price(40, 0)
+      checkoutService.basketTotalPrice(List(item, item), Set.empty) shouldBe Price(40)
+    }
+
+    "correctly apply a discount for an item" in {
+      checkoutService.basketTotalPrice(List(item, item, item), Set(someRandomItemDiscount)) shouldBe Price(55)
     }
   }
 }
